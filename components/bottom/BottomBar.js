097f5a539b593724afb95f32f214dfb6e345ba6e
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2022-09-04 23:47:42 +0600

    ok

--- components/bottom/BottomBar.js
+++ components/bottom/BottomBar.js
@@ -1,11 +1,49 @@
 import React from 'react';
-import {View} from 'react-native';
+import {
+    View, Text, ScrollView,
+    StyleSheet, TouchableOpacity, Image, SafeAreaView
+} from 'react-native';
+import {
+    FontAwesome, MaterialCommunityIcons,
+    Ionicons,
+} from '@expo/vector-icons';
 const BottomBar = () => {
     return (
-        <View>
-            
+        <View style={styles.Box}>
+            <TouchableOpacity style={{flex:1,marginLeft:20}}>
+                <FontAwesome name="stethoscope" size={50} color="white" />
+            </TouchableOpacity>
+
+            <TouchableOpacity style={{flex:1}}>
+                <MaterialCommunityIcons name="briefcase-clock" size={45} color="white" />
+            </TouchableOpacity>
+
+            <TouchableOpacity style={{flex:1}}>
+                <Ionicons name="md-home" size={45} color="white" />
+            </TouchableOpacity>
+
+            <TouchableOpacity style={{flex:1}}>
+                <Ionicons name="document-text" size={45} color="white" />
+            </TouchableOpacity>
+
+            <TouchableOpacity style={{flex:1}}>
+                <Ionicons name="md-menu" size={55} color="white" />
+            </TouchableOpacity>
         </View>
     );
 };
 
 export default BottomBar;
+
+const styles = StyleSheet.create({
+    Box: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        display: 'flex',
+        height: 60,
+        marginLeft: 5,
+        marginRight: 5,
+        backgroundColor: '#7267D1',
+        borderRadius: 15,
+    },
+})
