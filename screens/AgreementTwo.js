0f43a7c5d3bec50addd6eaf47106d4d84a9c7c90
Hasibur Rahman <81801818+hrtamim@users.noreply.github.com>
2022-10-06 12:40:46 +0600

    ok

--- screens/AgreementTwo.js
+++ screens/AgreementTwo.js
@@ -4,25 +4,12 @@ import { Ionicons } from '@expo/vector-icons';
 import { styles } from '../assets/styles';
 import Button from '../components/button/Button'
 import React, { useState, useEffect } from 'react';
-import * as ImagePicker from 'expo-image-picker';
+
 
 
 function AgreementTwo(props) {
     const [text, onChangeText] = React.useState(null);
-    const pickImage = async () => {
-        // No permissions request is necessary for launching the image library
-        let result = await ImagePicker.launchImageLibraryAsync({
-          mediaTypes: ImagePicker.MediaTypeOptions.All,
-          allowsEditing: true,
-          aspect: [4, 3],
-          quality: 1,
-        });
-    
-        console.log(result);
-    
-        if (!result.cancelled) {
-          setImage(result.uri);
-        }}
+  
     return (
         <View style={{margin:10}}>
             <Ionicons onPress={() => {
@@ -65,10 +52,7 @@ function AgreementTwo(props) {
 
                 />
             </View>
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button title="Pick an image from camera roll" onPress={pickImage} />
-      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
-    </View>
+            
             <View style={{alignItems:'center'}}>
                 <Button onPress={() => {props.navigation.navigate('AgreementThree')}} buttonName='NEXT' />
             </View>
